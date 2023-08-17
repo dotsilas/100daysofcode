0c1ea8860d89dd794c9f6ce6f29d13e570de8fc3
Silas Duarte <silas.duarte.h@gmail.com>
2023-08-17 10:14:09 -0600

    Add conditionals and functions Go notes

--- go-language-basics/main.go
+++ go-language-basics/main.go
@@ -63,23 +63,157 @@ func loops() {
 	}
 	fmt.Println("i's value is ", i)
 
-	// while-like loop with for
-	var n int
+	// while-like loop with for var n int
+	n := 3
 	for n < 5 {
 		fmt.Println("n is", n)
 		n++
 	}
 
 	// infinite loops
-	for {
-		fmt.Println("hello!")
+	// for {
+	// 	fmt.Println("hello!")
+	// }
+	
+	// use of break and continue
+	for i := 0; i < 10; i++ {
+		if i % 2 == 0 {
+			continue
+		}
+		fmt.Println(i)
 	}
-	// )
 }
+
+func conditionals() {
+	x := 3
+	// if statement
+	if x > 2 {
+		fmt.Println("x is greater than 2")
+	}
+
+	// if statement w/ init statement
+	// if err := someFunction(); err != nil {
+	// 	fmt.Print(err)
+	// }
+	
+	if true {
+		fmt.Println("Is true")
+	} else {
+		fmt.Println("Isn't true")
+	}
+
+	// "else" omition
+	// v, err := someFunc()
+	// if someFunc != nil {
+	// 	return err
+	// }
+	// fmt.Println(v)
+	// return nil
+	
+	// else if
+	if x > 0 {
+		fmt.Println("x is greater than 0")
+	} else if x < 0 {
+		fmt.Println("x is less than 0")
+	} else {
+		fmt.Println("x is equal to 0")
+	}
+
+	// switch statement
+	switch x {
+	case 3:
+		fmt.Println("x is 3")
+	case 4, 5:
+		fmt.Println("x is 4 or 5")
+	default: // in Go default is optional
+		fmt.Println("x is unknown")
+	}
+
+// switch with init statement
+	// switch x:= someFunction(); x {
+	// case "none":
+	// 	fmt.Println("yes")
+	// case "yes":
+	// 	fmt.Println("not yes")
+	// }
+	
+	switch {
+	case x > 0: // boolean evaluations only works if don't use match after switch
+		fmt.Println("x is greater than 0")
+	case x < 0:
+		fmt.Println("x is less than 0")
+	default:
+		fmt.Println("x is 0")
+	}
+}
+
+// FUNCTIONS
+// is not possible to declarate regular functions inside functions
+// but is possible to declarate anonimous functions inside other functions
+func add(x, y int) int {
+	return x + y
+}
+
+// functions with multiple return
+
+// multiple return must be placed between ()
+func divide(num, div int) (res, rem int) {
+	// res and rem don't use := because they are always created
+	res = num / div
+	rem = num % div
+	return res, rem
+}
+
+// variadic arguments
+func sum(numbers []int) int {
+	sum := 0
+	for _, n := range numbers {
+		sum += n
+	}
+	return sum
+}
+
+// variadic notation
+func sum2(numbers ...int) int {
+	sum := 0
+	for _, n := range numbers {
+		sum += n
+	}
+	return sum
+}
+
+// Public and Private
+aPrivateVariable := "hola" // lowerCase start letter
+APublicVariable := "" // UpperCase start letter
 
 func main() {
 	packages()
 	variables()
 	dataTypes()
 	loops()
+	conditionals()
+
+	result := add(10,20)
+	fmt.Printf("10 + 20 = %d\n", result)
+
+	// if you only wants one return, can use _ to not used
+	result, remainder := divide(10,3)
+	fmt.Printf("10 / 3 = %d and remainder %d\n", result, remainder)
+
+	sum2Result := sum2(1,2,3,4,5)
+	fmt.Println(sum2Result)
+
+	// anonimous function
+	greet := func() string {
+		return "Hello!"
+	}
+
+	fmt.Println(greet())
+
+	// create a single-use function and pass arguments
+	anotherGreet := func(word1, word2 string) string {
+		return word1 + " " + word2
+	} ("hola", "mundo")
+
+	fmt.Println(anotherGreet)
 }
