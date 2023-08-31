4f56b03c4726c835a91d4eba850a19e8cb0afe83
Silas Duarte <silas.duarte.h@gmail.com>
2023-08-31 11:08:13 -0600

    Go: errors, constants, defer, panic and recover notes

--- go-language-basics/essentials/main.go
+++ go-language-basics/essentials/main.go
@@ -2,6 +2,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"log"
 )
 
 // things that make Go different:
@@ -40,7 +41,39 @@ func (e ErrNetwork) Error() string {
 	return fmt.Sprintf("network error(%d): %s", e.Code, e.Msg)
 }
 
+// constants declaration
+// constants can be typed and untyped
+const aNumberConstant int64 = 10
+const theString = "A string"
+
+// constants as enumaration using 'iota' keyword
+const (
+	// iota automatically increase number for any new constant
+	a = iota // 0
+	b // 1
+	c // 2
+)
+
+// can be used with math operations
+const (
+	d = iota*2 // 0
+	e // 2
+	f // 4
+)
+
+// untyped const can be used as types of the same family, vars not
+type mySpecialStringtype string
+func SpecialPrint(s mySpecialStringtype) {
+	fmt.Println(string(s))
+}
+
 func main() {
+	const constString = "hello"
+	// var varString string = "hello"
+
+	SpecialPrint(constString)
+	// SpecialPrint(varString) // error string != mySpecialStringtype
+
 	dividedBy := []int{0,1,2,3}
 	for _, div := range dividedBy {
 		res, err := Divide(100, div)
@@ -50,4 +83,43 @@ func main() {
 		}
 		fmt.Printf("100 divided by %d = %d\n", div, res)
 	}
+
+	// detect an error with errors.As()
+	// var netErr ErrNetwork
+	// if errors.As(err, &netErr) {
+	// 	if netErr.Code == AuthFailureCode {
+	// 	log.Println("unrecoverable auth failure: ", err)
+	// 	break
+	// 	}
+	// 	log.Println("recoverable error: %s", netErr)
+	// }
+	// log.Println("unrecoverable error: %s", err)
+	// break
+}
+
+// defer
+// executes at the end of a function's execution process
+func printStuff() (value string) {
+	defer fmt.Println("exiting")
+	defer func() {
+		value = "we returned this"
+	}()
+	fmt.Println("I am printing stuff")
+	return ""
+}
+
+// panic
+// stops programm execution
+// panic("some bug ocurrence")
+
+// recover
+// recover from a panic
+func someFunc() {
+	defer func() {
+		// 
+		if r := recover(); r != nil {
+			log.Printf("called recover, panic was: %q", r)
+		}
+	}()
+	panic("oh no!!!")
 }
