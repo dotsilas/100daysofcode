bb06b0b2c81325a878ab91bb4357142cdbce73cf
Silas Duarte <silas.duarte.h@gmail.com>
2023-10-23 17:41:54 -0600

    canvas

--- web/canvas/script.js
+++ web/canvas/script.js
@@ -12,15 +12,35 @@ const mousePosition = {
   y: undefined,
 }
 
+canvas.addEventListener("click", (event) => {
+  mousePosition.x = event.x;
+  mousePosition.y = event.y;
+})
+
 canvas.addEventListener("mousemove", (event) => {
   mousePosition.x = event.x;
   mousePosition.y = event.y;
-  drawCircle();
 })
 
-function drawCircle(){
-  ctx.fillStyle = "red";
+function drawCircle() {
+  ctx.fillStyle = "blue";
   ctx.beginPath();
   ctx.arc(mousePosition.x, mousePosition.y, 3, 0, Math.PI * 2);
   ctx.fill();
 }
+
+class Particle {
+  constructor(){
+    self.x = mousePosition.x;
+    self.y = mousePosition.y;
+    size = Math.random() * 5 + 1;
+  }
+}
+
+let animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawCircle();
+  requestAnimationFrame(animate);
+}
+
+animate();
