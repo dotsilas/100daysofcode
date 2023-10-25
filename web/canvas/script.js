0e32e3acd4174279f2405b01d231d98e2e9e90ab
Silas Duarte <silas.duarte.h@gmail.com>
2023-10-24 22:00:58 -0600

    canvas

--- web/canvas/script.js
+++ web/canvas/script.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
+let ParticlesArray = [];
 console.log(ctx);
 
 window.addEventListener("resize", () => {
@@ -22,24 +23,45 @@ canvas.addEventListener("mousemove", (event) => {
   mousePosition.y = event.y;
 })
 
-function drawCircle() {
-  ctx.fillStyle = "blue";
-  ctx.beginPath();
-  ctx.arc(mousePosition.x, mousePosition.y, 3, 0, Math.PI * 2);
-  ctx.fill();
+class Particle {
+  constructor() {
+    // this.x = mousePosition.x;
+    // this.y = mousePosition.y;
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+  update() {
+    this.x = this.speedX;
+    this.y = this.speedY;
+  }
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 5, 0, Math.PI * 2);
+    ctx.fill();
+  }
 }
 
-class Particle {
-  constructor(){
-    self.x = mousePosition.x;
-    self.y = mousePosition.y;
-    size = Math.random() * 5 + 1;
+function init() {
+  for (let i = 0; i < 100; i++) {
+    ParticlesArray.push(new Particle());
+  }
+}
+init();
+
+function handleParticles() {
+  for (let i = 0; i < ParticlesArray.length; i++) {
+    ParticlesArray[i].update();
+    ParticlesArray[i].draw();
   }
 }
 
 let animate = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawCircle();
+  handleParticles();
   requestAnimationFrame(animate);
 }
 
